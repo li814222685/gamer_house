d587abe36de2eb4cfe25fadc12b1b05f764f7d3e
leehao <814222685@qq.com>
2022-07-29 00:25:36 +0800

    init store

---
+++ .umirc.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from '@umijs/max';
+
+export default defineConfig({
+  antd: {},
+  access: {},
+  model: {},
+  initialState: {},
+  request: {},
+  layout: {
+    title: '🎮 小屋',
+  },
+  routes: [
+    {
+      path: '/',
+      redirect: '/home',
+    },
+    {
+      name: '首页',
+      path: '/home',
+      component: './Home',
+    },
+    {
+      name: '权限演示',
+      path: '/access',
+      component: './Access',
+    },
+    {
+      name: ' CRUD 示例',
+      path: '/table',
+      component: './Table',
+    },
+  ],
+  npmClient: 'pnpm',
+});
